2f7d6e2c6b7504118451b13e9b76a4a06fd57f79
andrey-melnikov <gbandrewgb@gmail.com>
2020-02-27 19:25:09 +0300

    edit

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,19 @@
 import json
 from flask import Flask, jsonify, abort, request, render_template, session
-from Main import main
+from celery import Celery
 import datetime
+import Main
 
 application = Flask(__name__)
 application.secret_key="hello"
+application.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+application.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+
+celery = Celery(application.name, broker=application.config['CELERY_BROKER_URL'])
+celery.conf.update(application.config)
+
+
+
 
 @application.route("/")
 @application.route("/index")
@@ -19,13 +28,16 @@ def api_info():
 def example():
 	return render_template('example.html')
 
+
+
+
 @application.route("/api/v1.0/gen", methods=['POST'])
-def generate():
+def gen():
 	id_user = request.json['id_user']
 	with open('users.txt','r') as json_file:
 		if (id_user in json.load(json_file)) == False:
 			return  abort(404)
-	session['time'] = str(datetime.datetime.now())
+	session['file_id'] = str(id_user+" "+datetime.datetime.now())
 	id_target = request.json['id_target']
 	lang = request.json['lang']
 	reviews_amount = (int)(request.json['reviews_amount'])
@@ -36,16 +48,23 @@ def generate():
 	except:
 		similar_ids=[]
 
+	generating.delay(id_target,lang,reviews_amount,ratings,mode,session['file_id'],similar_ids)
 	return jsonify({'file_id':str(id_user+session['time'])})
-	main(id_target,lang,reviews_amount,ratings,mode,session['time'],similar_ids)
 	#return jsonify({'return':'Generating reviews for '+id_target}),201
+
+@celery.task
+def generating(id_target,lang,reviews_amount,ratings,mode,name, similar_ids):
+	Main.main(id_target,lang,reviews_amount,ratings,mode,name,similar_ids)
+
+
+
 
 
 @application.route("/api/v1.0/get_reviews", methods=['GET'])
 def get_reviews():
-	if 'time' in session:
+	if 'file_id' in session:
 		ratings = (int)(request.json['ratings'])
-		output_path = "output/reviews/"+session['time']+".txt"
+		output_path = "output/reviews/"+session['file_id']+".txt"
 		output_file = open(output_path, 'r',encoding='utf-8')
 		r = {}
 		i=0
@@ -57,6 +76,10 @@ def get_reviews():
 		return r
 	else:
 		return 'no session'
+
+
+
+
 
 @application.route("/api/v1.0/progress",methods=['GET'])
 def progress():
