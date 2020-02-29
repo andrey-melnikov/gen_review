f51812b106bca4316a55a0fbfe3d1098f966612e
andrey-melnikov <gbandrewgb@gmail.com>
2020-03-01 01:08:41 +0300

    edit

--- ClientEmulator.py
+++ ClientEmulator.py
@@ -12,7 +12,7 @@ def progress(task_id):
 		return (res.json())
 
 def post_id(dict):
-	res = requests.post('http://gen-review.net/api/v1.0/gen', json=dict)
+	res = requests.post('http://80.249.144.13:5000/api/v1.0/gen', json=dict)
 	if res.ok:
 		return (res.json())	
 
@@ -29,14 +29,14 @@ def similar(dict):
 
 if __name__ == "__main__":
 	dict = {'user_id':'user2',
-			'target_id': 'com.ansangha.drjb',
+			'target_id': 'com.AndreyMelnikov.DroneRacingSimulator',
 			'lang':'en_US',
 			'reviews_amount': 200,
 			'length': "Short",
 			'file_id':'user2 2020-02-27 22:53:44.456785',
 			'ratings':1}
 	#print(request_file_id({'user_id': 'user2'}))
-	#print(post_id(dict))
-	print(get_reviews({'file_id':'user2 2020-02-27 22:53:44.456785', 'ratings':"1"}))
+	print(post_id(dict))
+	#print(get_reviews({'file_id':'user2 2020-02-27 22:53:44.456785', 'ratings':"1"}))
 	#print(progress("f635e99d-7737-4498-b9ec-501316093de3"))
 	#print(similar({'target_id':'com.AndreyMelnikov.DroneRacingSimulator'}))
